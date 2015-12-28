4f7b18494c7a1125d961742382453d8efb62d55f
hectorlf <hectorlopezfernandez@hotmail.com>
2015-12-28 23:50:17 +0100

    Adapt test to neo4j's bolt protocol

--- Neo4jTest/src/main/java/test/Neo4jTest.java
+++ Neo4jTest/src/main/java/test/Neo4jTest.java
@@ -1,6 +1,5 @@
 package test;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
@@ -9,12 +8,14 @@ import org.neo4j.driver.v1.GraphDatabase;
 import org.neo4j.driver.v1.Node;
 import org.neo4j.driver.v1.ResultCursor;
 import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.Transaction;
+import org.neo4j.driver.v1.Value;
 import org.neo4j.driver.v1.Values;
 
 public class Neo4jTest {
 
-	private static final int USER_COUNT = 100;
-	private static final int MAX_CARDINALITY = 5;
+	private static final int USER_COUNT = 10000;
+	private static final int MAX_CARDINALITY = 500;
 	private static final float REL_PROBABILITY = 0.1f;
 	
 	private final Driver driver;
@@ -91,112 +92,83 @@ public class Neo4jTest {
 	
 
 	private void findWithCypherReflexiveRelationForUsers(int idA, int idB) {
-		Map<String,Object> params = new HashMap<String,Object>(2);
-		params.put("userId", Integer.valueOf(idA));
-		params.put("anotherId", Integer.valueOf(idB));
-		/*
-		Result result = graphDb.execute( "match (n:user{id:{userId}})-[r:LIKES]->(m:user{id:{anotherId}})-[r2:LIKES]->(n) return n.id limit 1", params );
-		ResourceIterator<Integer> ids = result.columnAs("n.id");
-		if (ids.hasNext()) {
+		Session ses = driver.session();
+		Map<String,Value> params = Values.parameters("userId", Integer.valueOf(idA), "anotherId", Integer.valueOf(idB));
+		ResultCursor result = ses.run("match (n:User{id:{userId}})-[:LIKES]->(m:User{id:{anotherId}})-[:LIKES]->(n) return n.id limit 1", params);
+		if (result.single()) {
 			System.out.println("Se ha encontrado una relacion reflexiva entre los usuarios " + idA + " y " + idB);
 		} else {
 			System.out.println("No se ha encontrado ninguna relacion reflexiva para los usuarios " + idA + " y " + idB);
 		}
-		*/
+		ses.close();
 	}
 
+
 	private void findWithCypherReflexiveRelationForUser(int idUser) {
-		Map<String,Object> params = new HashMap<String,Object>(1);
-		params.put("userId", Integer.valueOf(idUser));
-		/*
-		Result result = graphDb.execute( "match (n:user{id:{userId}})-[r:LIKES]->(m:user)-[r2:LIKES]->(n) return m.id limit 1", params );
-		ResourceIterator<Integer> ids = result.columnAs("m.id");
-		if (ids.hasNext()) {
-			Integer id = ids.next();
+		Session ses = driver.session();
+		Map<String,Value> params = Values.parameters("userId", Integer.valueOf(idUser));
+		ResultCursor result = ses.run("match (n:User{id:{userId}})-[r:LIKES]->(m:User)-[r2:LIKES]->(n) return m.id limit 1", params);
+		if (result.single()) {
+			Integer id = result.value(0).asInt();
 			System.out.println("Se ha encontrado una relacion entre los usuarios " + idUser + " y " + id);
 		} else {
 			System.out.println("No se ha encontrado ninguna relacion reflexiva para el usuario " + idUser);
 		}
-		*/
+		ses.close();
 	}
 
+
 	private void findWithCypherAnyReflexiveRelation() {
-		/*
-		Result result = graphDb.execute( "match (n:user)-[r:LIKES]->(m:user)-[r2:LIKES]->(n) return n.id, m.id limit 1" );
-		if (result.hasNext()) {
-			Map<String,Object> entry = result.next();
-			int foundIdA = ((Integer)entry.get("n.id")).intValue();
-			int foundIdB = ((Integer)entry.get("m.id")).intValue();
+		Session ses = driver.session();
+		ResultCursor result = ses.run("match (n:User)-[r:LIKES]->(m:User)-[r2:LIKES]->(n) return n.id, m.id limit 1");
+		if (result.single()) {
+			int foundIdA = result.value(0).asInt();
+			int foundIdB = result.value(1).asInt();
 			System.out.println("Se ha encontrado una relacion reflexiva entre los usuarios " + foundIdA + " y " + foundIdB);
 		} else {
 			System.out.println("No se ha encontrado una relacion reflexiva entre ningun par de usuarios");
 		}
-		*/
-	}
-	
-	private void findMostLiked() {
-		int[] likes = new int[USER_COUNT];
-		/*
-		ResourceIterator<Node> nodes = graphDb.findNodes(userLabel);
-		while (nodes.hasNext()) {
-			Node n = nodes.next();
-			likes[(int)n.getId()] = n.getDegree(RelTypes.LIKES, Direction.INCOMING);
-		}
-		int highestValue = 0;
-		int highestLiked = 0;
-		for (int i = 0; i < likes.length; i++) {
-			if (highestValue < likes[i]) {
-				highestValue = likes[i];
-				highestLiked = i;
-			}
-		}
-		System.out.println("El usuario con mayor numero de likes recibidos es " + highestLiked + ", con " + highestValue + " relaciones");
-		*/
-	}
-	private void findMostLiker() {
-		int[] likes = new int[USER_COUNT];
-		/*
-		ResourceIterator<Node> nodes = graphDb.findNodes(userLabel);
-		while (nodes.hasNext()) {
-			Node n = nodes.next();
-			likes[(int)n.getId()] = n.getDegree(RelTypes.LIKES, Direction.OUTGOING);
-		}
-		int highestValue = 0;
-		int highestLiker = 0;
-		for (int i = 0; i < likes.length; i++) {
-			if (highestValue < likes[i]) {
-				highestValue = likes[i];
-				highestLiker = i;
-			}
-		}
-		System.out.println("El usuario con mayor numero de likes realizados es " + highestLiker + ", con " + highestValue + " relaciones");
-		*/
+		ses.close();
 	}
 
-	
+
+	private void findMostLiked() {
+		Session ses = driver.session();
+		ResultCursor result = ses.run("match (n:User)<--() return n.id, count(*) as degree order by degree desc limit 1");
+		result.first();
+		System.out.println("El usuario con mayor numero de likes recibidos es " + result.value(0).asInt() + ", con " + result.value(1).asInt() + " relaciones");
+		ses.close();
+	}
+	private void findMostLiker() {
+		Session ses = driver.session();
+		ResultCursor result = ses.run("match (n:User)-->() return n.id, count(*) as degree order by degree desc limit 1");
+		result.first();
+		System.out.println("El usuario con mayor numero de likes enviados es " + result.value(0).asInt() + ", con " + result.value(1).asInt() + " relaciones");
+		ses.close();
+	}
+
+
 	private void findWithCypherSimilarLikesForUser(int idUser) {
-		Map<String,Object> params = new HashMap<String,Object>(1);
-		params.put("userId", Integer.valueOf(idUser));
-		/*
-		Result result = graphDb.execute("match (n:user{id:{userId}})<-[r:LIKES]-(m:user)-[r2:LIKES]->(t:user) where t <> n return distinct t.id, count(t) as num order by num desc, t.id asc limit 5", params );
-		ResourceIterator<Integer> ids = result.columnAs("t.id");
-		if (ids.hasNext()) {
+		Session ses = driver.session();
+		Map<String,Value> params = Values.parameters("userId", Integer.valueOf(idUser));
+		ResultCursor result = ses.run("match (n:User{id:{userId}})<-[:LIKES]-(m:User)-[:LIKES]->(t:User) where t <> n return distinct t.id, count(t) as num order by num desc, t.id asc limit 5", params);
+		if (result.size() > 0) {
 			System.out.print("Se han encontrado los siguientes usuarios similares para el usuario " + idUser + ": ");
-			while (ids.hasNext()) {
-				Integer id = ids.next();
-				System.out.print(id + " ");
+			while (result.next()) {
+				System.out.print(result.value(0).asInt() + " ");
 			}
 			System.out.println();
 		} else {
 			System.out.println("No se ha encontrado ning�n usuario similar para el usuario " + idUser);
 		}
-		*/
+		ses.close();
 	}
 
-	
-	
+
+
 	private void initialize() {
 		Session ses = driver.session();
+		Transaction tx = null;
 		Random rand = new Random(System.currentTimeMillis());
 		
 		ResultCursor result = ses.run("match (n:User) return count(n)");
@@ -205,34 +177,47 @@ public class Neo4jTest {
 			return;
 		}
 
+		System.out.println("Creando indices");
+		ses.run("create index on :User(id)");
+		
 		System.out.println("Creando usuarios");
+		tx = ses.beginTransaction();
 		for (int i = 0; i < USER_COUNT; i++) {
-			ses.run("create (n:User { id: {id} })", Values.parameters("id", Integer.valueOf(i)));
-			System.out.println("Usuarios creados: " + i);
-			/*
+			tx.run("create (n:User { id: {id} })", Values.parameters("id", Integer.valueOf(i)));
 			if (i % 2000 == 0) {
 				tx.success();
 				tx.close();
-				tx = graphDb.beginTx();
+				tx = ses.beginTransaction();
 				System.out.println("Usuarios creados: " + i);
 			}
-			*/
 		}
+		tx.success();
+		tx.close();
+		System.out.println("Usuarios creados: " + USER_COUNT);
 		
 		System.out.println("Creando relaciones");
 		int numRels = 0;
+		tx = ses.beginTransaction();
 		for (int i = 0; i < USER_COUNT; i++) {
 			for (int j = 0; j < MAX_CARDINALITY; j++) {
 				if (rand.nextFloat() < REL_PROBABILITY) {
 					Node user2 = getNode(rand.nextInt(USER_COUNT));
 					if (user2 != null) {
-						ses.run("match (n:User {id:{nid}}), (m:User {id:{mid}}) with n,m create (n)-[:LIKES]->(m)", Values.parameters("nid", Integer.valueOf(i), "mid", user2.value("id").asInt()));
+						tx.run("match (n:User {id:{nid}}), (m:User {id:{mid}}) with n,m create (n)-[:LIKES]->(m)", Values.parameters("nid", Integer.valueOf(i), "mid", user2.value("id").asInt()));
 						numRels++;
+						if (numRels % 5000 == 0) {
+							tx.success();
+							tx.close();
+							tx = ses.beginTransaction();
+							System.out.println("Relaciones creadas: " + numRels);
+						}
 					}
-					System.out.println("Relaciones creadas: " + numRels);
 				}
 			}
 		}
+		tx.success();
+		tx.close();
+		System.out.println("Relaciones creadas: " + numRels);
 	}
 
 	private Node getNode(Integer id) {
